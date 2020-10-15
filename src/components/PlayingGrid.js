907493764ff070ab5af265bbe1ef5117dedaa3c5
shawn-nyk <shawnngyongkwang@gmail.com>
2020-10-16 04:02:20 +0800

    Add game logic for checking for winner

--- src/components/PlayingGrid.js
+++ src/components/PlayingGrid.js
@@ -6,67 +6,93 @@ class PlayingGrid extends React.Component {
   constructor() {
     super();
     this.state = {
-      ownerArr: [["none", "none", "none"], ["none", "none", "none"], ["none", "none", "none"]],
+      gameState: [["none", "none", "none"], ["none", "none", "none"], ["none", "none", "none"]],
+      gameOver: false
     };
   }
 
   onClickHandler(r, c) {
-    let newOwnerArr = [...this.state.ownerArr];
-    newOwnerArr[r][c] = "player";
-    this.setState({ownerArr: newOwnerArr});
-    if (!this.isGameOver()) {
-      this.opponentMoves();
+    let newGameState = [...this.state.gameState];
+    let gameOver = this.state.gameOver;
+    if (gameOver || newGameState[r][c] != "none") {
+      return;
     }
+    newGameState[r][c] = "player";
+    if (this.checkWinner(newGameState) || this.checkBoardFilled(newGameState)) {
+      gameOver = true;
+    }
+    if (!gameOver) {
+      newGameState = this.opponentMoves(newGameState);
+    }
+    this.setState({gameState: newGameState, gameOver: gameOver});
   }
 
-  isGameOver() {
-    let gameOver = true;
+  checkWinner(gameState) {
+    let haveWinner = false;
+    for (let i = 0; i < 3; i++) {
+      if ((gameState[0][i] != "none") && (gameState[0][i] == gameState[1][i]) && (gameState[1][i] == gameState[2][i])) {
+        haveWinner = true;
+      }
+      if ((gameState[i][0] != "none") && (gameState[i][0] == gameState[i][1]) && (gameState[i][1] == gameState[i][2])) {
+        haveWinner = true;
+      }
+    }
+    if ((gameState[0][0] != "none") && (gameState[0][0] == gameState[1][1]) && (gameState[1][1] == gameState[2][2])) {
+      haveWinner = true;
+    }
+    if ((gameState[0][2] != "none") && (gameState[0][2] == gameState[1][1]) && (gameState[1][1] == gameState[2][0])) {
+      haveWinner = true;
+    }
+    return haveWinner;
+  }
+
+  checkBoardFilled(gameState) {
+    let boardFilled = true;
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        if (this.state.ownerArr[i][j] == "none") {
-          gameOver = false;
+        if (gameState[i][j] == "none") {
+          boardFilled = false;
           break;
         }
       }
-      if (!gameOver) {
+      if (!boardFilled) {
         break;
       }
     }
-    return gameOver;
+    return boardFilled;
   }
 
-  opponentMoves() {
-    let newOwnerArr = [...this.state.ownerArr];
+  opponentMoves(newGameState) {
     let randomRow = 0;
     let randomCol = 0;
     do {
       randomRow = Math.round(Math.random() * 2);
       randomCol = Math.round(Math.random() * 2);
-    } while (newOwnerArr[randomRow][randomCol] != "none");
-    newOwnerArr[randomRow][randomCol] = "opponent";
-    this.setState({ownerArr: newOwnerArr});
+    } while (newGameState[randomRow][randomCol] != "none");
+    newGameState[randomRow][randomCol] = "opponent";
+    return newGameState;
   }
 
-  reset() {
-    let newOwnerArr = [["none", "none", "none"], ["none", "none", "none"], ["none", "none", "none"]];
-    this.setState({ownerArr: newOwnerArr});
+  newGame() {
+    let newGameState = [["none", "none", "none"], ["none", "none", "none"], ["none", "none", "none"]];
+    this.setState({gameState: newGameState, gameOver: false});
   }
 
   render() {
     return (
       <>
         <div id="playing-grid">
-          <PlayingSquare id="square-1" owner={this.state.ownerArr[0][0]} onClickHandler={() => this.onClickHandler(0, 0)}/>
-          <PlayingSquare id="square-2" owner={this.state.ownerArr[0][1]} onClickHandler={() => this.onClickHandler(0, 1)}/>
-          <PlayingSquare id="square-3" owner={this.state.ownerArr[0][2]} onClickHandler={() => this.onClickHandler(0, 2)}/>
-          <PlayingSquare id="square-4" owner={this.state.ownerArr[1][0]} onClickHandler={() => this.onClickHandler(1, 0)}/>
-          <PlayingSquare id="square-5" owner={this.state.ownerArr[1][1]} onClickHandler={() => this.onClickHandler(1, 1)}/>
-          <PlayingSquare id="square-6" owner={this.state.ownerArr[1][2]} onClickHandler={() => this.onClickHandler(1, 2)}/>
-          <PlayingSquare id="square-7" owner={this.state.ownerArr[2][0]} onClickHandler={() => this.onClickHandler(2, 0)}/>
-          <PlayingSquare id="square-8" owner={this.state.ownerArr[2][1]} onClickHandler={() => this.onClickHandler(2, 1)}/>
-          <PlayingSquare id="square-9" owner={this.state.ownerArr[2][2]} onClickHandler={() => this.onClickHandler(2, 2)}/>
+          <PlayingSquare id="square-1" owner={this.state.gameState[0][0]} onClickHandler={() => this.onClickHandler(0, 0)}/>
+          <PlayingSquare id="square-2" owner={this.state.gameState[0][1]} onClickHandler={() => this.onClickHandler(0, 1)}/>
+          <PlayingSquare id="square-3" owner={this.state.gameState[0][2]} onClickHandler={() => this.onClickHandler(0, 2)}/>
+          <PlayingSquare id="square-4" owner={this.state.gameState[1][0]} onClickHandler={() => this.onClickHandler(1, 0)}/>
+          <PlayingSquare id="square-5" owner={this.state.gameState[1][1]} onClickHandler={() => this.onClickHandler(1, 1)}/>
+          <PlayingSquare id="square-6" owner={this.state.gameState[1][2]} onClickHandler={() => this.onClickHandler(1, 2)}/>
+          <PlayingSquare id="square-7" owner={this.state.gameState[2][0]} onClickHandler={() => this.onClickHandler(2, 0)}/>
+          <PlayingSquare id="square-8" owner={this.state.gameState[2][1]} onClickHandler={() => this.onClickHandler(2, 1)}/>
+          <PlayingSquare id="square-9" owner={this.state.gameState[2][2]} onClickHandler={() => this.onClickHandler(2, 2)}/>
         </div>
-        <button id="reset-button" onClick={() => this.reset()}>reset</button>
+        <button id="reset-button" onClick={() => this.newGame()}>new game</button>
       </>
     );
   }
