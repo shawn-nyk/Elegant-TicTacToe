5d9b2b3e33184def0a5778e1c92c738142521c04
shawn-nyk <shawnngyongkwang@gmail.com>
2020-10-16 13:49:29 +0800

    Add game logic

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import PlayingGrid from "./components/PlayingGrid";
 function App() {
   return (
     <div className="App">
+      <div id="title">
+        <h1 id="title-solid">TIC TAC TOE</h1>
+        <h1 id="title-deco">TIC TAC TOE</h1>
+      </div>
       <PlayingGrid />
     </div>
   );
--- src/components/PlayingGrid.js
+++ src/components/PlayingGrid.js
@@ -7,43 +7,52 @@ class PlayingGrid extends React.Component {
     super();
     this.state = {
       gameState: [["none", "none", "none"], ["none", "none", "none"], ["none", "none", "none"]],
-      gameOver: false
+      gameOver: false,
+      winner: "none"
     };
   }
 
   onClickHandler(r, c) {
     let newGameState = [...this.state.gameState];
     let gameOver = this.state.gameOver;
+    let winner = this.state.winner;
     if (gameOver || newGameState[r][c] != "none") {
       return;
     }
     newGameState[r][c] = "player";
-    if (this.checkWinner(newGameState) || this.checkBoardFilled(newGameState)) {
+    winner = this.checkWinner(newGameState);
+    if (winner != "none" || this.checkBoardFilled(newGameState)) {
       gameOver = true;
     }
     if (!gameOver) {
       newGameState = this.opponentMoves(newGameState);
     }
-    this.setState({gameState: newGameState, gameOver: gameOver});
+    winner = this.checkWinner(newGameState);
+    if (winner != "none" || this.checkBoardFilled(newGameState)) {
+      gameOver = true;
+    }
+    this.setState({gameState: newGameState, gameOver: gameOver, winner: winner});
   }
 
   checkWinner(gameState) {
-    let haveWinner = false;
+    let winner = "none";
     for (let i = 0; i < 3; i++) {
       if ((gameState[0][i] != "none") && (gameState[0][i] == gameState[1][i]) && (gameState[1][i] == gameState[2][i])) {
-        haveWinner = true;
+        winner = gameState[0][i];
+        break;
       }
       if ((gameState[i][0] != "none") && (gameState[i][0] == gameState[i][1]) && (gameState[i][1] == gameState[i][2])) {
-        haveWinner = true;
+        winner = gameState[i][0];
+        break;
       }
     }
     if ((gameState[0][0] != "none") && (gameState[0][0] == gameState[1][1]) && (gameState[1][1] == gameState[2][2])) {
-      haveWinner = true;
+      winner = gameState[0][0];
     }
     if ((gameState[0][2] != "none") && (gameState[0][2] == gameState[1][1]) && (gameState[1][1] == gameState[2][0])) {
-      haveWinner = true;
+      winner = gameState[0][2];
     }
-    return haveWinner;
+    return winner;
   }
 
   checkBoardFilled(gameState) {
@@ -75,12 +84,15 @@ class PlayingGrid extends React.Component {
 
   newGame() {
     let newGameState = [["none", "none", "none"], ["none", "none", "none"], ["none", "none", "none"]];
-    this.setState({gameState: newGameState, gameOver: false});
+    this.setState({gameState: newGameState, gameOver: false, winner: "none"});
   }
 
   render() {
+    const visibility = this.state.gameOver ? "visible" : "hidden";
+    const gameOverMessage = this.state.winner != "player" ? "Better luck next time!" : "Congratulations! You won!"; 
     return (
       <>
+        <h1 style={{visibility: visibility}} id="game-over-title">{gameOverMessage}</h1>
         <div id="playing-grid">
           <PlayingSquare id="square-1" owner={this.state.gameState[0][0]} onClickHandler={() => this.onClickHandler(0, 0)}/>
           <PlayingSquare id="square-2" owner={this.state.gameState[0][1]} onClickHandler={() => this.onClickHandler(0, 1)}/>
@@ -92,7 +104,7 @@ class PlayingGrid extends React.Component {
           <PlayingSquare id="square-8" owner={this.state.gameState[2][1]} onClickHandler={() => this.onClickHandler(2, 1)}/>
           <PlayingSquare id="square-9" owner={this.state.gameState[2][2]} onClickHandler={() => this.onClickHandler(2, 2)}/>
         </div>
-        <button id="reset-button" onClick={() => this.newGame()}>new game</button>
+        <button id="reset-button" onClick={() => this.newGame()}>New Game</button>
       </>
     );
   }
